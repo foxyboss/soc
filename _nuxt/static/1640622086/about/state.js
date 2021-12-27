47e10f137f5028745644aa7b6cf06b792fb9c8bc
foxyboss <foxyboss@users.noreply.github.com>
2021-12-27 16:21:27 +0000

    deploy: bebc6a5962298d52a1a43106e0057b1b9ef25eaa

---
+++ _nuxt/static/1640622086/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsoc\u002F_nuxt\u002Fstatic\u002F1640622086",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fsoc\u002F",assetsPath:"\u002Fsoc\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"43312e64"}}}}(null));
