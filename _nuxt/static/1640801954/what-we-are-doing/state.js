0488dd2ff3e8c642254c534b2fdbbafec00ed5cb
foxyboss <foxyboss@users.noreply.github.com>
2021-12-29 18:19:17 +0000

    deploy: bba63e925ec32b9d42c036786f7e8cad6cf760f1

---
+++ _nuxt/static/1640801954/what-we-are-doing/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsoc\u002F_nuxt\u002Fstatic\u002F1640801954",layout:"empty",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002Fsoc\u002F",assetsPath:"\u002Fsoc\u002F_nuxt\u002F",cdnURL:null},content:{dbHash:"cdfdec5d"}}}}("\u002Fwhat-we-are-doing"));
